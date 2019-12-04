ddea1b8046e731b34e614338854424514ff3271a
CUI DI <cuidi.me@gmail.com>
2019-12-04 12:48:57 +0900

    bug fixed

--- src/components/SelectNetworks.tsx
+++ src/components/SelectNetworks.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import s from "./SelectNetworks.module.scss";
 import networksConfig from "../config/networks";
@@ -8,25 +8,23 @@ type Props = {
   className?: string;
 };
 const SelectNetworks = (props: Props) => {
+  const { network, setNetwork } = useContext(NetworkContext);
+
   return (
-    <NetworkContext.Consumer>
-      {({ network, setNetwork }) => (
-        <div className={props.className}>
-          <select
-            className={s.select}
-            value={network}
-            onChange={e => setNetwork(e.target.value)}
-          >
-            {networksConfig.map(({ key }, index) => {
-              return <option key={index}>{key}</option>;
-            })}
-          </select>
-          <div className={s.addon}>
-            <i className="material-icons">arrow_drop_down</i>
-          </div>
-        </div>
-      )}
-    </NetworkContext.Consumer>
+    <div className={props.className}>
+      <select
+        className={s.select}
+        value={network}
+        onChange={e => setNetwork(e.target.value)}
+      >
+        {networksConfig.map(({ key }, index) => {
+          return <option key={index}>{key}</option>;
+        })}
+      </select>
+      <div className={s.addon}>
+        <i className="material-icons">arrow_drop_down</i>
+      </div>
+    </div>
   );
 };
 
--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,35 +1,18 @@
-import React, { Component } from "react";
-
+import React from "react";
 import routes from "../routes";
 import s from "./App.module.scss";
 import Header from "./Header";
 import ErrorBoundary from "../components/ErrorBoundary";
 
-import NetworkContext from "../contexts/NetworkContext";
-
-class App extends Component {
-  setNetwork = (network: string) => {
-    this.setState({
-      network: network
-    });
-  };
-  state = {
-    network: window.location.pathname.split("/")[1],
-    setNetwork: this.setNetwork
-  };
-
-  render() {
-    return (
-      <section className={s.main}>
-        <NetworkContext.Provider value={this.state}>
-          <Header />
-          <section className={s.content}>
-            <ErrorBoundary>{routes}</ErrorBoundary>
-          </section>
-        </NetworkContext.Provider>
+const App = () => {
+  return (
+    <section className={s.main}>
+      <Header />
+      <section className={s.content}>
+        <ErrorBoundary>{routes}</ErrorBoundary>
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
 export default App;
