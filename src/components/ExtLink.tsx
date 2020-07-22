8b0c22da879738944b694543a4e5d10f83e6166e
sim <sim@terra.money>
2020-07-22 15:56:18 +0900

    feature: Link repo url of contract

---
+++ src/components/ExtLink.tsx
@@ -0,0 +1,23 @@
+import React, { FC, AnchorHTMLAttributes } from "react";
+
+type Anchor = AnchorHTMLAttributes<HTMLAnchorElement>;
+const ExtLink: FC<Anchor> = ({ href, children, ...attrs }) =>
+  href ? (
+    <a {...attrs} href={fix(href)} target="_blank" rel="noopener noreferrer">
+      {children}
+    </a>
+  ) : (
+    <span {...attrs}>{children}</span>
+  );
+
+export default ExtLink;
+
+/* helper */
+const fix = (href: string): string => {
+  try {
+    new URL(href!);
+    return href ?? "";
+  } catch (error) {
+    return href ? `https://${href}` : "";
+  }
+};
