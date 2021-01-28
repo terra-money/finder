d124cf65dcc4e928cf429522e08787b249f53127
James <james@terra.money>
2021-01-28 12:31:55 +0900

    feature(tokens): Show new tokens icon

--- src/pages/Account/Available.tsx
+++ src/pages/Account/Available.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import format from "../../scripts/format";
 import AmountCard from "./AmountCard";
 
-const Available = ({ denom, available }: Balance) => {
-  return (
-    <>
-      <AmountCard denom={format.denom(denom)} amount={available} />
-    </>
-  );
-};
+const Available = ({ denom, available }: Balance) => (
+  <AmountCard denom={format.denom(denom)} amount={available} />
+);
 
 export default Available;
