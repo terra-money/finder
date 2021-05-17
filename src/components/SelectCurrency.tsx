67cab8fe3cabe6c581d5600d17134719394de3b9
James <james@terra.money>
2021-05-17 13:33:17 +0900

    feature(currency): Add currency conversion to native tokens in account page

---
+++ src/components/SelectCurrency.tsx
@@ -0,0 +1,45 @@
+import { useContext, useEffect } from "react";
+import { useRequest } from "../HOCs/WithFetch";
+import CurrencyContext from "../contexts/CurrencyContext";
+import s from "./SelectCurrency.module.scss";
+import { getCookie } from "../scripts/cookie";
+import { DEFAULT_CURRENCY, getDefaultCurrency } from "../scripts/utility";
+
+type Props = {
+  className?: string;
+};
+
+const SelectCurrency = (props: Props) => {
+  const { currency, selectCurrency } = useContext(CurrencyContext);
+  const response: ActiveDenom = useRequest({ url: `/oracle/denoms/actives` });
+  const currencyArray = response.data?.result.filter(str => str !== "uluna");
+
+  const denom = currencyArray?.includes(currency) ? currency : DEFAULT_CURRENCY;
+
+  useEffect(() => {
+    if (!getCookie("currency") && currencyArray) {
+      const currency = getDefaultCurrency(currencyArray);
+      selectCurrency(currency);
+    }
+  }, [selectCurrency, currencyArray]);
+
+  return (
+    <div className={props.className}>
+      <select
+        className={s.select}
+        value={denom.substr(1).toUpperCase()}
+        onChange={e => selectCurrency(`u${e.target.value}`.toLowerCase())}
+      >
+        {currencyArray?.map((currency, key) => {
+          const activeDenom = currency.substr(1).toUpperCase();
+          return <option key={key}>{activeDenom}</option>;
+        })}
+      </select>
+      <div className={s.addon}>
+        <i className="material-icons">arrow_drop_down</i>
+      </div>
+    </div>
+  );
+};
+
+export default SelectCurrency;
--- src/pages/Account/AvailableList.tsx
+++ src/pages/Account/AvailableList.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { useContext } from "react";
+import CurrencyContext from "../../contexts/CurrencyContext";
+import { useRequest } from "../../HOCs/WithFetch";
+import { DEFAULT_CURRENCY } from "../../scripts/utility";
 import Available from "./Available";
 
-const AvailableList = ({ list }: { list: Balance[] }) => (
-  <>
-    {list.map((a, i) => (
-      <Available {...a} key={i} />
-    ))}
-  </>
-);
+const AvailableList = ({ list }: { list: Balance[] }) => {
+  const { currency } = useContext(CurrencyContext);
+
+  const denomResponse: ActiveDenom = useRequest({
+    url: `/oracle/denoms/actives`
+  });
+
+  const denom = denomResponse.data?.result?.includes(currency)
+    ? currency
+    : DEFAULT_CURRENCY;
+
+  const response = useRequest({
+    url: `/v1/market/swaprate/${denom}`
+  });
+
+  return (
+    <>
+      {list.map((a, i) => (
+        <Available
+          {...a}
+          key={i}
+          currency={{ response: response, currency: denom }}
+        />
+      ))}
+    </>
+  );
+};
 
 export default AvailableList;
