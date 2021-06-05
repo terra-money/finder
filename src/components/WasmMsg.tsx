e8c9c161bbf4815dea3390bdbf1573c5da03e8a4
Jesse Chung <kjessec@users.noreply.github.com>
2021-06-06 00:29:36 +0900

    fix: prettify wasm (#95)
    * fix: recursive prettifier
    
    * fix: extend is-base64
    
    * chore: remove debug messages
    
    * chore: little comment for isBase64Extended
    
    * chore: prettifyWasmMsg reviver function => arrow function
    
    * fix: isBase64Extended should only parse json-like base64
    
    * fix: remove unnecessary check
    
    * fix: remove unnecessary .toString()

--- src/components/WasmMsg.tsx
+++ src/components/WasmMsg.tsx
@@ -1,19 +1,23 @@
 import isBase64 from "is-base64";
 import { decodeBase64 } from "../scripts/utility";
 
+const isBase64Extended = (value: string) =>
+  typeof value === "string" &&
+  // we are only interested in json-alike base64's, which generally start with "ey" ('{')
+  value.startsWith("ey") &&
+  // other checks
+  isBase64(value);
+
+const reviver = (_: string, value: any) =>
+  isBase64Extended(value) ? JSON.parse(decodeBase64(value), reviver) : value;
+
 const prettifyWasmMsg = (str: string | object) => {
-  if (typeof str === "string" && isBase64(str)) {
+  if (typeof str === "string" && isBase64Extended(str)) {
     const decoded = decodeBase64(str);
     try {
-      const parsed = JSON.parse(decoded);
-      if (typeof parsed === "object") {
-        Object.keys(parsed).forEach(key => {
-          parsed[key].msg = JSON.parse(decodeBase64(parsed[key].msg));
-        });
-      }
-      return JSON.stringify(parsed, undefined, 2);
-    } catch (e) {
-      return JSON.stringify(decoded, undefined, 2);
+      return JSON.stringify(JSON.parse(decoded, reviver), null, 2);
+    } catch (_) {
+      return decoded;
     }
   }
   return JSON.stringify(str, undefined, 2);
