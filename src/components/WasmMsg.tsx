bd40a6e0ced456c3ca25228e2f496012fc6b510f
jess <jesse@terra.money>
2021-06-06 00:09:24 +0900

    fix: remove unnecessary check

--- src/components/WasmMsg.tsx
+++ src/components/WasmMsg.tsx
@@ -1,12 +1,9 @@
 import isBase64 from "is-base64";
 import { decodeBase64 } from "../scripts/utility";
 
-// is-base64 takes terra1 address as base64 as well; prevent this.
 const isBase64Extended = (value: string) =>
   typeof value === "string" &&
-  // case: terra1... address is bech32 and it naturally is base64-eligible.
-  !value.startsWith("terra1") &&
-  // case: we are only interested in json-alike base64's, which generally start with "ey" ('{')
+  // we are only interested in json-alike base64's, which generally start with "ey" ('{')
   value.startsWith("ey") &&
   // other checks
   isBase64(value);
