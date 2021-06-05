ef869b603ac9ee84801b370e433240a3f4423ecb
jess <jesse@terra.money>
2021-06-05 23:49:05 +0900

    fix: recursive prettifier

--- src/components/WasmMsg.tsx
+++ src/components/WasmMsg.tsx
@@ -1,19 +1,17 @@
 import isBase64 from "is-base64";
 import { decodeBase64 } from "../scripts/utility";
 
+function reviver(_: string, value: any) {
+  return isBase64(value) ? JSON.parse(decodeBase64(value), reviver) : value;
+}
+
 const prettifyWasmMsg = (str: string | object) => {
   if (typeof str === "string" && isBase64(str)) {
     const decoded = decodeBase64(str);
     try {
-      const parsed = JSON.parse(decoded);
-      if (typeof parsed === "object") {
-        Object.keys(parsed).forEach(key => {
-          parsed[key].msg = JSON.parse(decodeBase64(parsed[key].msg));
-        });
-      }
-      return JSON.stringify(parsed, undefined, 2);
-    } catch (e) {
-      return JSON.stringify(decoded, undefined, 2);
+      return JSON.stringify(JSON.parse(decoded.toString(), reviver), null, 2);
+    } catch (_) {
+      return decoded;
     }
   }
   return JSON.stringify(str, undefined, 2);
