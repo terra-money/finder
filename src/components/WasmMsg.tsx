b62c0968a04adb7665c72eacfcf9ba10038e599a
Jesse Chung <kjessec@users.noreply.github.com>
2021-06-06 03:37:36 +0900

    chore: type correction, cleanup (#96)
    * chore: type correction, cleanup
    
    * fix: move string typecheck to elsewhere

--- src/components/WasmMsg.tsx
+++ src/components/WasmMsg.tsx
@@ -2,21 +2,22 @@ import isBase64 from "is-base64";
 import { decodeBase64 } from "../scripts/utility";
 
 const isBase64Extended = (value: string) =>
-  typeof value === "string" &&
   // we are only interested in json-alike base64's, which generally start with "ey" ('{')
   value.startsWith("ey") &&
   // other checks
   isBase64(value);
 
 const reviver = (_: string, value: any) =>
-  isBase64Extended(value) ? JSON.parse(decodeBase64(value), reviver) : value;
+  typeof value === "string" && isBase64Extended(value)
+    ? JSON.parse(decodeBase64(value), reviver)
+    : value;
 
 const prettifyWasmMsg = (str: string | object) => {
   if (typeof str === "string" && isBase64Extended(str)) {
     const decoded = decodeBase64(str);
     try {
       return JSON.stringify(JSON.parse(decoded, reviver), null, 2);
-    } catch (_) {
+    } catch {
       return decoded;
     }
   }
