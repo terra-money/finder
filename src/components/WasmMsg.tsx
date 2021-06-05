d5b65bd5c313d7130db06123cf9e8f37966358bb
jess <jesse@terra.money>
2021-06-06 00:06:34 +0900

    fix: isBase64Extended should only parse json-like base64

--- src/components/WasmMsg.tsx
+++ src/components/WasmMsg.tsx
@@ -3,7 +3,13 @@ import { decodeBase64 } from "../scripts/utility";
 
 // is-base64 takes terra1 address as base64 as well; prevent this.
 const isBase64Extended = (value: string) =>
-  typeof value === "string" && !value.startsWith("terra1") && isBase64(value);
+  typeof value === "string" &&
+  // case: terra1... address is bech32 and it naturally is base64-eligible.
+  !value.startsWith("terra1") &&
+  // case: we are only interested in json-alike base64's, which generally start with "ey" ('{')
+  value.startsWith("ey") &&
+  // other checks
+  isBase64(value);
 
 const reviver = (_: string, value: any) =>
   isBase64Extended(value) ? JSON.parse(decodeBase64(value), reviver) : value;
