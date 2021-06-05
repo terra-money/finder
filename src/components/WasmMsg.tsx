92a03346316505dd9d5dd6e6da26a04c65de02ab
jess <jesse@terra.money>
2021-06-05 23:57:17 +0900

    fix: extend is-base64

--- src/components/WasmMsg.tsx
+++ src/components/WasmMsg.tsx
@@ -1,16 +1,22 @@
 import isBase64 from "is-base64";
 import { decodeBase64 } from "../scripts/utility";
 
+const isBase64Extended = (value: string) =>
+  typeof value === "string" && !value.startsWith("terra1") && isBase64(value);
+
 function reviver(_: string, value: any) {
-  return isBase64(value) ? JSON.parse(decodeBase64(value), reviver) : value;
+  return isBase64Extended(value)
+    ? JSON.parse(decodeBase64(value), reviver)
+    : value;
 }
 
 const prettifyWasmMsg = (str: string | object) => {
-  if (typeof str === "string" && isBase64(str)) {
+  if (typeof str === "string" && isBase64Extended(str)) {
     const decoded = decodeBase64(str);
     try {
       return JSON.stringify(JSON.parse(decoded.toString(), reviver), null, 2);
     } catch (_) {
+      console.log(_);
       return decoded;
     }
   }
