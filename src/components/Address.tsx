c1cc11a0fffca542cf94d18b6c9e5153475d78b9
James <james@terra.money>
2021-09-09 10:08:30 +0900

    chore: refactor css

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -44,14 +44,16 @@ const formatAccAddress = (
           {showProtocolName && (
             <span className={s.protocol}>{contract.protocol}</span>
           )}
-          <Finder q="address" v={address} children={names} />
-          {hideIcon ? undefined : (
-            <img
-              src={token?.icon || contract?.icon}
-              alt={token?.symbol || contract?.name}
-              className={s.icon}
-            />
-          )}
+          <div className={s.addressWrapper}>
+            <Finder q="address" v={address} children={names} />
+            {hideIcon ? undefined : (
+              <img
+                src={token?.icon || contract?.icon}
+                alt={token?.symbol || contract?.name}
+                className={s.icon}
+              />
+            )}
+          </div>
         </>
       ) : (
         <Finder q="address" v={address} children={renderAddress} />
