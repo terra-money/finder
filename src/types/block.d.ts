6b75221a5fd000022da64db25e220c0151520ccf
James <james@terra.money>
2021-10-01 00:49:51 +0900

    remove old type

--- src/types/block.d.ts
+++ src/types/block.d.ts
@@ -8,26 +8,6 @@ interface BlockHeader {
   };
 }
 
-interface OldBlock {
-  // Before Columbus-3 there was block_meta
-  block_meta?: {
-    block_id: {
-      hash: string;
-    };
-    header: BlockHeader;
-  };
-  // Columbus-4 has block_id in first depth
-  block_id?: {
-    hash: string;
-  };
-  block: {
-    header: BlockHeader;
-    data: {
-      txs: string[];
-    };
-  };
-}
-
 interface Block {
   chainId: string;
   height: number;
