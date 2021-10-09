b777a5a563f5bea00b34eaab71a0eda58545bc07
James <james@terra.money>
2021-10-09 14:50:12 +0900

    fix: Handle icon issue

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -5,16 +5,14 @@ import SelectNetworks from "../components/SelectNetworks";
 
 import { Link } from "react-router-dom";
 import SelectCurrency from "../components/SelectCurrency";
+import logo from "../images/logo.svg";
 
 const Header = () => (
   <div className={s.header}>
     <div className={s.inner}>
       <div className={s.logo}>
         <Link to="/">
-          <img
-            src="https://s3.ap-northeast-2.amazonaws.com/terra.money.home/static/finder/logo.svg"
-            alt=""
-          />
+          <img src={logo} alt="logo" />
         </Link>
       </div>
       <Search className={s.search} />
