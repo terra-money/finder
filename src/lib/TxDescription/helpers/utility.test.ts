9ad8235cc0444f38a3fedeace5d7f3b74f7baf7e
James0126 <james@terra.money>
2021-11-09 16:26:49 +0900

    feature: Use react-widget (#169)

--- src/lib/TxDescription/helpers/utility.test.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-import { isCoins } from "./utility";
-
-test("isCoins", () => {
-  expect(isCoins("1uluna")).toBeTruthy();
-  expect(isCoins("1uluna,")).toBeTruthy();
-  expect(isCoins("1uluna,1uusd")).toBeTruthy();
-});
