098223211488f78d28c713825d114ae77ee3a7f1
James0126 <james@terra.money>
2021-11-11 17:50:04 +0900

    Remove sentry

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,5 +1,4 @@
 import { Component, ReactNode } from "react";
-import * as Sentry from "@sentry/browser";
 
 const OOPS = "Oops! Something went wrong.";
 
@@ -7,13 +6,6 @@ type Props = { fallback?: ReactNode };
 class ErrorBoundary extends Component<Props> {
   state = { hasError: null };
   static getDerivedStateFromError = () => ({ hasError: true });
-
-  componentDidCatch(error: Error, errorInfo: { [key: string]: any }) {
-    Sentry.withScope(scope => {
-      scope.setExtras(errorInfo);
-      Sentry.captureException(error);
-    });
-  }
 
   render() {
     const { fallback = OOPS, children } = this.props;
