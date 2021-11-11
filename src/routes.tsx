c02bdc2375a922662b54c51a51e4328d8e819670
James0126 <james@terra.money>
2021-11-11 19:18:05 +0900

    feature: Add block url path

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,6 +11,7 @@ export default (
   <Routes>
     <Route index element={<Index />} />
     <Route path="blocks/:height" element={<Block />} />
+    <Route path="block/:height" element={<Block />} />
     <Route path="txs/:height" element={<Block />} />
     <Route path="tx/:hash" element={<Tx />} />
     <Route path="validator/:address" element={<Validator />} />
