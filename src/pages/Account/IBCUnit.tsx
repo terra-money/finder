541298338dc2627a88d7072145fe9c6b9b16aaad
James0126 <james@terra.money>
2022-06-13 19:18:59 +0900

    Fix ibc data issue

--- src/pages/Account/IBCUnit.tsx
+++ src/pages/Account/IBCUnit.tsx
@@ -31,9 +31,9 @@ export default IBCUnit;
 /* hook */
 export const useIBCWhitelist = () => {
   const chainID = useCurrentChain();
-  const { data } = useQuery(["IBCWhitelist"], () =>
+  const { data } = useQuery(["IBCWhitelist", chainID], () =>
     axios.get(`${ASSET_URL}/ibc/tokens.json`)
   );
 
-  return data?.data[chainID.name];
+  return data?.data?.[chainID.name];
 };
