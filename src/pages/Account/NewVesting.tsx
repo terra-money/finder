e7459c408b524565e2fee2e2d05abd2beef493fb
James0126 <james@terra.money>
2022-06-14 12:10:41 +0900

    Change vesting design (#230)
    * Change vesting design
    
    * Refactor vesting design

--- src/pages/Account/NewVesting.tsx
+++ src/pages/Account/NewVesting.tsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
 import { useQuery } from "react-query";
-import Icon from "../../components/Icon";
+import c from "classnames/bind";
 import Amount from "../../components/Amount";
 import useLCDClient from "../../hooks/useLCD";
 import { useAccountInfo } from "../../queries/auth";
 import { RefetchOptions } from "../../queries/query";
 import format from "../../scripts/format";
+import { div, percent } from "../../scripts/math";
 import getVesting from "../../scripts/vesting";
 import AmountCard from "./AmountCard";
 import Schedule from "./Schedule";
 import VestingCard from "./VestingCard";
 import s from "./Vesting.module.scss";
 
+const cx = c.bind(s);
+
 const NewVesting = ({ address }: { address: string }) => {
   const { data: accountInfo } = useAccountInfo(address);
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(isOpen => !isOpen);
-
   const { data: block } = useLatestBlock();
   const info = accountInfo?.toData();
 
@@ -31,31 +30,36 @@ const NewVesting = ({ address }: { address: string }) => {
   }
 
   const { total, denom, totalReleased, schedules } = vestingInfo;
+  const releasedPercent = div(Number(totalReleased), total);
+  const isPeriodicVestingAccount = schedules.length > 1;
+
   return (
     <VestingCard>
-      <AmountCard
-        denom={format.denom(denom)}
-        amount={total}
-        button={
-          <button onClick={toggle} className={s.button}>
-            <Icon name={isOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"} />
-          </button>
-        }
-      >
-        {isOpen && (
-          <>
-            {Number(totalReleased) > 0 ? (
-              <h1>
-                Total Released: <Amount denom={denom}>{totalReleased}</Amount>
-              </h1>
-            ) : null}
-            <section className={s.schedules}>
-              {schedules.map((s, i) => (
-                <Schedule {...s} denom={denom} key={i} />
-              ))}
-            </section>
-          </>
-        )}
+      <AmountCard denom={format.denom(denom)} amount={total}>
+        <article
+          className={cx({
+            wrapper: !isPeriodicVestingAccount,
+            rowReverse: Number(totalReleased) > 1
+          })}
+        >
+          {Number(totalReleased) > 1 ? (
+            <h1 className={s.released}>
+              <span className={s.text}>Released:</span>{" "}
+              <Amount denom={denom}>{totalReleased}</Amount> |{" "}
+              {percent(releasedPercent)}
+            </h1>
+          ) : null}
+          <section className={cx({ schedules: isPeriodicVestingAccount })}>
+            {schedules.map((s, i) => (
+              <Schedule
+                {...s}
+                isPeriodicVestingAccount={isPeriodicVestingAccount}
+                key={i}
+                index={i}
+              />
+            ))}
+          </section>
+        </article>
       </AmountCard>
     </VestingCard>
   );
